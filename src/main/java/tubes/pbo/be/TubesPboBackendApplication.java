402d89fa4b4863a720f149c6100d7f770545dbb4
WaxArsatia <waxarsatia@gmail.com>
2026-01-03 23:58:45 +0700

    feat: Init
    Signed-off-by: WaxArsatia <waxarsatia@gmail.com>

---
+++ src/main/java/tubes/pbo/be/TubesPboBackendApplication.java
@@ -0,0 +1,13 @@
+package tubes.pbo.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesPboBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubesPboBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tubes/pbo/be/TubesPboBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tubes.pbo.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubesPboBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
